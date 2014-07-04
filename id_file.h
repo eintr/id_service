4e75ca33f6d8f3326d3979032964dd8c072049ac
牛海青 <haiqing1@staff.sina.com.cn>
2014-07-04 17:35:42 +0800

    Added spin_lock operation.

--- id_file.h
+++ id_file.h
@@ -8,7 +8,12 @@ extern int id_array_size;
 
 int id_file_load(const char *fname);
 
+struct id_entry_st *id_file_append(const char *name, uint64_t start);
+
 void id_file_sync(void);
+
+void id_file_spin_lock(void);
+void id_file_spin_unlock(void);
 
 #endif
 
