6a6b35d43de3fd601c879b7af94724f5112209bc
牛海青 <haiqing1@staff.sina.com.cn>
2014-07-08 10:59:59 +0800

    Expand the buffer if needed.

--- id_msg_header.c
+++ id_msg_header.c
@@ -6,16 +6,20 @@
 
 #include "id_msg_header.h"
 
+#define	BUF_SIZE_INIT	1024
+#define	BUF_SIZE_EXPAND	4096
+
 int id_msg_recv_generic(conn_tcp_t *conn, struct id_msg_buf_st *b, int version, int command)
 {
     struct id_msg_header_st *hdr;
+	void *newbuf;
     struct epoll_event ev;
     ssize_t len;
 
 	if (b->buf==NULL) {
 		b->buf_size = conn_tcp_suggest_bufsize(conn)*2;
-		if (b->buf_size < 4096) {
-			b->buf_size = 4096;
+		if (b->buf_size < BUF_SIZE_INIT) {
+			b->buf_size = BUF_SIZE_INIT;
 		}
 		b->buf = calloc(b->buf_size, 1);
 		if (b->buf == NULL) {
@@ -26,40 +30,48 @@ int id_msg_recv_generic(conn_tcp_t *conn, struct id_msg_buf_st *b, int version, 
 	}
 
 	if (b->buf_size - b->len <1) {
-		/* \todo expand the buffer if needed. */
-		fprintf(stderr, "%s: Buffer is too small, complete this TODO at once!\n", __FUNCTION__);
-		abort();
+		/* expand the buffer if needed. */
+		newbuf = realloc(b->buf, b->buf_size + BUF_SIZE_EXPAND);
+		if (newbuf==NULL) {
+			mylog(L_ERR, "Memory allocation failed!");
+			return RCV_ERROR;
+		}
+		mylog(L_DEBUG, "Buffer size (%zd) is too small, expaned to %zd.", b->buf_size, b->buf_size+BUF_SIZE_EXPAND);
+		b->buf_size += BUF_SIZE_EXPAND;
+		b->buf = newbuf;
 	}
+
+
 	len = conn_tcp_recv_nb(conn, b->buf + b->pos, b->buf_size - b->len);
 	if (len<0) {
 		if (errno==EAGAIN) {
-			fprintf(stderr, "%s: errno==EAGAIN\n", __FUNCTION__);
+			//fprintf(stderr, "%s: errno==EAGAIN\n", __FUNCTION__);
 			return RCV_WAIT;
 		} else {
-			fprintf(stderr, "%s: errno==%d\n", __FUNCTION__, errno);
+			//fprintf(stderr, "%s: errno==%d\n", __FUNCTION__, errno);
 			return RCV_ERROR;
 		}
 	} else if (len==0) {
-		fprintf(stderr, "%s: peer closed.\n", __FUNCTION__);
+		//fprintf(stderr, "%s: peer closed.\n", __FUNCTION__);
 		return RCV_ERROR;
 	} else {
 		b->len += len;
 		if (b->len<sizeof(struct id_msg_header_st)) {
-			fprintf(stderr, "%s: msg header is not completed, again.\n", __FUNCTION__);
+			//fprintf(stderr, "%s: msg header is not completed, again.\n", __FUNCTION__);
 			return RCV_CONTINUE;
 		}
 		hdr = (void*)(b->buf);
 		if (b->len < sizeof(struct id_msg_header_st)+ ntohl(hdr->body_length)) {
-			fprintf(stderr, "%s: msg body is not completed, again.\n", __FUNCTION__);
+			//fprintf(stderr, "%s: msg body is not completed, again.\n", __FUNCTION__);
 			return RCV_CONTINUE;
 		}
 		fprintf(stderr, "%s: msg recv completed.\n", __FUNCTION__);
 		if (hdr->version != version) {
-			fprintf(stderr, "%s: msg version==%d, Drop it!\n", __FUNCTION__, hdr->version);
+			//fprintf(stderr, "%s: msg version==%d, Drop it!\n", __FUNCTION__, hdr->version);
 			return RCV_ERROR;
 		}
 		if (command>0 && hdr->command != command) {
-			fprintf(stderr, "%s: msg command==%d, which I don't know how to deal with. Drop it!\n", __FUNCTION__, hdr->command);
+			//fprintf(stderr, "%s: msg command==%d, which I don't know how to deal with. Drop it!\n", __FUNCTION__, hdr->command);
 			return RCV_ERROR;
 		}
 		b->hdr = (void*)b->buf;
